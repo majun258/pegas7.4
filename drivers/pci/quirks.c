4842e12205387c0a8f6af4195e1651f83b7452fc
Robert Richter <rrichter@redhat.com>
2017-03-13 20:06:22 -0300

    [pci] vulcan: AHCI PCI bar fix for Broadcom Vulcan early silicon
    Message-id: <1489435582-21118-2-git-send-email-rrichter@redhat.com>
    Patchwork-id: 174072
    O-Subject: [Pegas 1.0 BZ 1430377 1/1] PCI: Vulcan: AHCI PCI bar fix for Broadcom Vulcan early silicon
    Bugzilla: 1430377
    RH-Acked-by: Mark Langsdorf <mlangsdo@redhat.com>
    RH-Acked-by: Jon Masters <jcm@redhat.com>
    RH-Acked-by: Mark Salter <msalter@redhat.com>
    
    From: Ashok Kumar Sekar <asekar@redhat.com>
    
    Brew: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=11758713
    Testing: Tested the patch against snapshot-1 kernel source
    Upstream: RHEL only
    
    PCI BAR 5 is not setup correctly for the on-board AHCI
    controller on Broadcom's Vulcan processor. Added a quirk to fix BAR 5
    by using BAR 4's resources which are populated correctly but NOT used
    by the AHCI controller actually.
    
    Signed-off-by: Ashok Kumar Sekar <asekar@redhat.com>
    Acked-by: Jon Masters <jcm@redhat.com>
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>
    [ from rhelsa7/master branch ]
    (cherry picked from commit 4c24bdb7b70c24274a6305e8c0f1fc05e9420381)
    Signed-off-by: Robert Richter <rrichter@redhat.com>
    
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -3958,6 +3958,30 @@ DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_INTEL, 0x2260, quirk_mic_x200_dma_alias);
 DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_INTEL, 0x2264, quirk_mic_x200_dma_alias);
 
 /*
+ * PCI BAR 5 is not setup correctly for the on-board AHCI
+ * controller on Broadcom's Vulcan processor. Added a quirk to fix BAR 5
+ * by using BAR 4's resources which are populated correctly and NOT used by
+ * AHCI controller actually. 
+ */
+static void quirk_fix_vulcan_ahci_bars(struct pci_dev *dev)
+{
+	struct resource *r =  &dev->resource[4];
+
+	if (!(r->flags & IORESOURCE_MEM) || (r->start == 0))
+		return;
+
+	/* Set BAR5 resource to BAR4 */
+	dev->resource[5] = *r;
+
+	/* Update BAR5 in pci config space */
+	pci_write_config_dword(dev, PCI_BASE_ADDRESS_5, r->start);
+
+	/* Clear BAR4's resource */
+	memset(r, 0, sizeof(*r));
+}
+DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_BROADCOM, 0x9027, quirk_fix_vulcan_ahci_bars);
+
+/*
  * Intersil/Techwell TW686[4589]-based video capture cards have an empty (zero)
  * class code.  Fix it.
  */
