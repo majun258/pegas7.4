d441f38a1949230aab081a4f6376150bf65eed8b
Jonathan Toppins <jtoppins@redhat.com>
2017-03-15 21:33:27 -0300

    [arm64] topology: Adjust sysfs topology
    Message-id: <2610fc56ac4ed81273de06a080dc4625a0220a30.1489613188.git.jtoppins@redhat.com>
    Patchwork-id: 174383
    O-Subject: [Pegas 1.0 RESUBMIT] [arm64] topology: Adjust sysfs topology
    Bugzilla: 1392076
    RH-Acked-by: Prarit Bhargava <prarit@redhat.com>
    RH-Acked-by: Andrew Jones <drjones@redhat.com>
    RH-Acked-by: Al Stone <ahs3@redhat.com>
    
    Extracting topology information from the MPIDR Affx fields as it
    currently defaults to generates the improper information under sysfs.
    DT has a format, if provided, albeit that is under an RFC as part of the
    arm64 NUMA support, and wouldn't work for RHELSA.  ACPI is developing
    new structures to provide the proper info, but is a bit off before its
    arrival.  Should be able to get some info from SMBIOS (dmi_scan_xxx),
    but firmware is tbd at the moment.
    
    This patch is a placeholder that should work for the known
    ARM64 SoCs expected to be supported in RHELSA over the next
    couple months until a final ACPI-based and/or SMBIOS3
    solution is in place.
    
    Note: On x86, the vast majority of this information is provided
    via architectured, cpuid invocations, and for really large systems,
    partial ACPI information.  There is no such equivalent on arm64
    until the ACPI proposal is approved with the associated kernel
    parsing support.
    
    The patch adds a new function that is run at late_initcall() time
    that scans the MPIDR.Affx fields that were read and stored for each
    cpu as they were brought online. Needed to wait for all cpus to be
    onlined as the MPIDR is per-cpu and don't know number of clusters
    (Aff1, Aff2 values) until after all cpus onlines. late_initcall()
    is after all cpus are online and before sysfs can be mount and read
    to retrieve topology information.
    
    This patch also supports parsing the MPIDR of multi-socket systems.  The
    socket ID is in the next higher affinity level than the cluster ID, and
    we use it to populate physical_package_id. We still effectively remove
    cluster_id as it's too late to expose that information for 7.3. We can't
    leave the zeroing of it though, because update_siblings_masks() uses it
    alone to determine core siblings. I.e. cluster_id needs to be an ID that
    is unique across sockets, otherwise we'll get inter- socket siblings. We
    can just use the newly extracted socket ID for cluster_id to solve that.
    
    Upstream Status: DO NOT UPSTREAM - there is a separate upstream effort to
                     fix this in a generic way, this patch should be dropped
                     once that's finished.
    
    Testing:
    On Mustang and ThunderX systems, running lscpu.
    
    On a Mustang:
    Before the kernel change, lscpu looked like this:
    Architecture:          aarch64
    Byte Order:            Little Endian
    CPU(s):                8
    On-line CPU(s) list:   0-7
    Thread(s) per core:    1
    Core(s) per socket:    2
    Socket(s):             4
    NUMA node(s):          1
    L1d cache:             32K
    L1i cache:             32K
    L2 cache:              256K
    NUMA node0 CPU(s):     0-7
    
    After the kernel changes, the output looks like:
    Architecture:          aarch64
    Byte Order:            Little Endian
    CPU(s):                8
    On-line CPU(s) list:   0-7
    Thread(s) per core:    1
    Core(s) per socket:    8
    Socket(s):             1
    NUMA node(s):          1
    NUMA node0 CPU(s):     0-7
    
    This is a squashed patch series from RHELSA 7.3, the original series
    consisted of (RHELSA commit ids):
      87334a34b39 ("DO NOT UPSTREAM: redhat: arm64: topology: Adjust sysfs topology")
      9c532e71b91 ("arm64: don't state topo is unsupported if !acpi")
      b5aa6b56205 ("arm64: topology: Correct core_id calculation")
      285a8e2f0fa ("DO NOT UPSTREAM: cputopo: support multiple sockets")
    
    Bugzilla: 1392076
    Build Info: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=12776889
    
    Signed-off-by: Jonathan Toppins <jtoppins@redhat.com>
    Signed-off-by: Herton R. Krzesinski <herton@redhat.com>

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -24,6 +24,7 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/cpufreq.h>
+#include <linux/acpi.h>
 
 #include <asm/cpu.h>
 #include <asm/cputype.h>
@@ -462,6 +463,8 @@ static void update_siblings_masks(unsigned int cpuid)
 	}
 }
 
+static int num_threads  = -1;
+static int num_cores    = -1;
 void store_cpu_topology(unsigned int cpuid)
 {
 	struct cpu_topology *cpuid_topo = &cpu_topology[cpuid];
@@ -491,6 +494,11 @@ void store_cpu_topology(unsigned int cpuid)
 					 MPIDR_AFFINITY_LEVEL(mpidr, 2) << 8 |
 					 MPIDR_AFFINITY_LEVEL(mpidr, 3) << 16;
 	}
+	/* For adjustment to topology later; see RHELSA topology adjustment below */
+	if (cpuid_topo->thread_id > num_threads)
+		num_threads = cpuid_topo->thread_id;
+	if (cpuid_topo->core_id > num_cores)
+		num_cores = cpuid_topo->core_id;
 
 	pr_debug("CPU%u: cluster %d core %d thread %d mpidr %#016llx\n",
 		 cpuid, cpuid_topo->cluster_id, cpuid_topo->core_id,
@@ -510,6 +518,7 @@ static void __init reset_cpu_topology(void)
 		cpu_topo->thread_id = -1;
 		cpu_topo->core_id = 0;
 		cpu_topo->cluster_id = -1;
+		cpu_topo->physical_package_id = 0;
 
 		cpumask_clear(&cpu_topo->core_sibling);
 		cpumask_set_cpu(cpu, &cpu_topo->core_sibling);
@@ -529,3 +538,52 @@ void __init init_cpu_topology(void)
 	if (of_have_populated_dt() && parse_dt_topology())
 		reset_cpu_topology();
 }
+
+/*
+ * RHELSA topology adjustment
+ *  -- DT-based topology not used
+ *  -- default use of MPIDR.Affx fields generates
+ *     incorrect topology information as coded today.
+ *  Need to work create ACPI-based topology information
+ *  but need time to work through standards.
+ *  For now, this will do for early chip partners.
+ *  Note: vcpus now use Aff1 and Aff0 (but still not more than 123 vcpus).
+ *        Since we flatten the topology, except sockets, and still don't
+ *        support multi-socket VMs this will also work in the VM case.
+ */
+static int __init acpi_adjust_cpu_topology(void)
+{
+	/* Adjust for case when ACPI is enabled */
+	if (!acpi_disabled) {
+		unsigned int cpu;
+
+		/* adjust counts to be base 1 vs base 0 */
+		num_threads++;	/* num threads per core */
+		num_cores++;	/* num cores per cluster */
+
+		for_each_possible_cpu(cpu) {
+			struct cpu_topology *cpu_topo = &cpu_topology[cpu];
+			int socket_id = (cpu_topo->cluster_id >> 8) & 0xff;
+			int cluster_id = cpu_topo->cluster_id & 0xff;
+
+			cpu_topo->core_id = cpu_topo->thread_id * num_threads +
+						cluster_id * num_cores +
+						cpu_topo->core_id;
+			cpu_topo->cluster_id = socket_id; /* flatten clusters for now */
+			cpu_topo->physical_package_id = socket_id;
+
+			/* reset sibling masks */
+			cpumask_clear(&cpu_topo->core_sibling);
+			cpumask_clear(&cpu_topo->thread_sibling);
+		}
+
+		/* update sibling masks */
+		for_each_possible_cpu(cpu)
+			update_siblings_masks(cpu);
+	}
+
+	return 0;
+}
+/* adjust topology after all cpu's started up and Affx fields extracted from per-cpu MPIDR */
+late_initcall(acpi_adjust_cpu_topology);
+
